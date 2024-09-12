2a05f019c5ef38d0b0e45b116ea573e74daf2d1e
jurazodda <juraevbehruz05@gmail.com>
2024-09-13 00:07:49 +0500

    updated models and added them to AutoMigrate

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,51 +1,48 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Username          string `gorm:"unique; not null"`
-	Email             string `gorm:"unique; not null"`
-	PasswordHash      string `gorm:"not null"`
-	IsBlocked         bool   `gorm:"default:false"`
-	PrifilePictureURL string
-	Bio               string
-	Posts             []Post     `gorm:"foreignKey:AuthorID"`
-	Comments          []Comment  `gorm:"foreignKey:AuthorID"`
-	Reactions         []Reaction `gorm:"foreignKey:AuthorID"`
-	Favorites         []Favorite `gorm:"foreignKey:AuthorID"`
+	Username     string `gorm:"unique; not null"`
+	Email        string `gorm:"unique; not null"`
+	PasswordHash string `gorm:"not null"`
+	AvatarURL    *string
+	Posts        []Post    `gorm:"foreignKey:AuthorID"`
+	Comments     []Comment `gorm:"foreignKey:AuthorID"`
 }
 
 type Post struct {
 	gorm.Model
-	Title          string     `gorm:"not null"`
-	Content        string     `gorm:"not null"`
-	AuthorID       uint       `gorm:"not null"`
-	RepostedFromID *uint      `gorm:"index"`
-	Author         User       `gorm:"foreignKey:AuthorID"`
-	Comments       []Comment  `gorm:"foreignKey:PostID"`
-	Reactions      []Reaction `gorm:"foreignKey:PostID"`
-	Favorites      []Favorite `gorm:"foreignKey:PostID"`
+	UserID   uint   `gorm:"not null"`
+	Title    string `gorm:"not null"`
+	Content  string `gorm:"not null"`
+	ImageURL string
 }
 
 type Comment struct {
 	gorm.Model
-	Content  string `gorm:"not null"`
-	PostID   uint   `gorm:"not null"`
-	AuthorID uint   `gorm:"not null"`
-	Post     Post   `gorm:"foreignKey:PostID"`
-	Author   User   `gorm:""foreignKey:AuthorID`
+	UserID  uint
+	PostID  uint
+	Content string
 }
 
-type Reaction struct {
-	gorm.Model
-	PostID uint   `gorm:"not null"`
-	UserID uint   `gorm:"not null"`
-	Type   string `gorm:"not null"`
-	Post   Post   `gorm:"foreignKey:PostID"`
-	User   User   `gorm:"foreignKey:UserID"`
+type Like struct {
+	ID        uint `gorm:"primaryKey"`
+	UserID    uint
+	PostID    uint
+	CreatedAt time.Time
+}
+
+type Repost struct {
+	ID        uint `gorm:"primaryKey"`
+	PostID    uint
+	UserID    uint
+	CreatedAt time.Time
 }
 
 type Favorite struct {
--- internal/repository/db.go
+++ internal/repository/db.go
@@ -8,16 +8,24 @@ import (
 	"gorm.io/gorm"
 )
 
-func DBConnection(dsn string) (DB *gorm.DB, err error) {
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func DBConnection(dsn string) (db *gorm.DB, err error) {
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("error connection to database", err)
 	}
 
-	err = DB.AutoMigrate(&models.User{})
-	if err != nil  {
+	err = db.AutoMigrate(
+		&models.User{},
+		&models.Post{},
+		&models.Comment{},
+		&models.Like{},
+		&models.Repost{},
+		&models.Favorite{},
+	)
+
+	if err != nil {
 		log.Fatal("error migrating database", err)
-	} 
+	}
 
 	return
 }
